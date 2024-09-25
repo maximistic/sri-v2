baf06849c453ec452d5b53116cac2155080e214e
maximistic <110153672+maximistic@users.noreply.github.com>
2024-09-25 19:45:02 +0530

    configs

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './Sections/Navbar.jsx';
+import Hero from './Sections/Hero.jsx';
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero/>
+    </main>
+  )
+}
+
+export default App
