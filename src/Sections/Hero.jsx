bc3487dc55a9e3bcc6f2cbcc1102d21c83cca4fd
maximistic <110153672+maximistic@users.noreply.github.com>
2024-09-25 19:46:04 +0530

    update

---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,18 @@
+const Hero = () => (
+  <section className="min-h-screen w-full flex flex-col relative">
+    <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 gap-3">
+      <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+        Hey There <span className="waving-hand">👋</span> Sri Here
+      </p>
+      <p className="hero_tag text-gray_gradient">
+        Breaking and Building Stuff
+      </p>
+
+      <div className="w-full h-full absolute inset-0">
+
+      </div>
+    </div>
+  </section>
+);
+
+export default Hero;
