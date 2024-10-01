d7d8ffd8d7844f28a97e964b60307f8033ff67b3
maximistic <110153672+maximistic@users.noreply.github.com>
2024-10-01 14:05:37 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,13 @@ import Hero from './Sections/Hero.jsx';
 import About from './Sections/About.jsx';
 import Contact from './Sections/Contact.jsx';
 import Footer from './Sections/Footer.jsx';
-import Experience from './Sections/Experience.jsx';
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
       <Hero/>
-
       <About />
-      <Experience />
       <Contact />
       <Footer />
     </main>
