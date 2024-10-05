23f8ffa5959ea82bc0db1d1076f9cee31f81e728
maximistic <110153672+maximistic@users.noreply.github.com>
2024-10-05 20:37:11 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Sri-Portfolio2/',
+  base: '/Sri-Portfolio2/', // GitHub Pages repo name
   build: {
     outDir: 'dist',
     rollupOptions: {
@@ -10,10 +10,8 @@ export default defineConfig({
         main: './index.html',
         404: './404.html',
       },
-      external: ['@react-three/fiber'],
+      external: ['@react-three/fiber', '@react-three/drei'], // Externalize both modules
     },
   },
   plugins: [react()],
 });
-
-
