511ba79aa2efc363426f3c84a18881afcbf0c72b
maximistic <110153672+maximistic@users.noreply.github.com>
2024-10-05 19:44:01 +0530

    models

--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -7,7 +7,6 @@ import { Canvas } from '@react-three/fiber';
 import CanvasLoader from '../Components/Loading';
 import DemoComputer from '../Components/DemoComputer';
 import { Center, OrbitControls } from "@react-three/drei";
-import { Group } from "three/examples/jsm/libs/tween.module.js";
 
 const projectCount = myProjects.length;
 
@@ -33,29 +32,31 @@ const Projects = () => {
   return (
     <section className="c-space my-20">
       <p className="grid-headtext">My Recent Works</p>
-      <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 w-full">
+
+      <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
         <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
           <div className="absolute top-0 right-0">
             <img src={currentProject.spotlight} alt="spotlight" className="w-full h-96 object-cover rounded-xl "/>
           </div>
 
           <div className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg" style={currentProject.logoStyle}>
-            <img src={currentProject.logo} alt="logo" className="w-10 h-10 shadow-sm " />
+            <img className="w-10 h-10 shadow-sm" src={currentProject.logo} alt="logo" />
           </div>
 
-          <div className="flex flexq-col gap-5 text-white-600 my-5">
-            <p className="text-white text-2xl font-semibold animatedText"> {currentProject.title} </p>
-            <p className="animatedText"> {currentProject.description} </p>
-            <p className="animatedText"> {currentProject.subdesc} </p>
+          <div className="flex flex-col gap-5 text-white-600 my-5">
+            <p className="text-white text-2xl font-semibold animatedText">{currentProject.title}</p>
+
+            <p className="animatedText">{currentProject.desc}</p>
+            <p className="animatedText">{currentProject.subdesc}</p>
           </div>
 
           <div className="flex items-center justify-between flex-wrap gap-5">
-            <div className="flex items-center gap-3"> 
+            <div className="flex items-center gap-3">
               {currentProject.tags.map((tag, index) => (
-                <div key={index} className="tech-logo"> 
-                  <img src={tag.path} alt={tag.name}/> 
+                <div key={index} className="tech-logo">
+                  <img src={tag.path} alt={tag.name} />
                 </div>
-              ))} 
+              ))}
             </div>
 
             <a className="flex items-center gap-2 cursor-pointer text-white-600" href={currentProject.href} target="_blank" rel="noreferrer">
@@ -76,16 +77,17 @@ const Projects = () => {
         </div>
 
         <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
-          <Canvas>
-            <ambientLight intensity={1} />
+        <Canvas>
+            <ambientLight intensity={Math.PI} />
             <directionalLight position={[10, 10, 5]} />
             <Center>
-              <Suspense fallback={<CanvasLoader/>}>
-                {/* <Group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
-                    <DemoComputer texture={currentProject.texture} />
-                </Group> */}
+              <Suspense fallback={<CanvasLoader />}>
+                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                  <DemoComputer texture={currentProject.texture} />
+                </group>
               </Suspense>
             </Center>
+            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={true} />
           </Canvas>
         </div>
 
