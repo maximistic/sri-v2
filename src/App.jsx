792961ad99f85f58c360c06cf7589fdbc53cf73e
maximistic <110153672+maximistic@users.noreply.github.com>
2024-10-22 19:21:12 +0530

    avatar

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,32 @@ import Experience from './Sections/Experience.jsx';
 import Projects from './Sections/Projects.jsx';
 import Certificates from './Sections/Certifications.jsx';
 import TechStacks from './Sections/TechStacks.jsx';
+import { Canvas } from '@react-three/fiber';
+import Exp from './Sections/Exp.jsx';
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
+      <Hero />
 
-      <Hero/>
+      <Canvas
+        shadows
+        camera={{ position: [0, 0, 8], fov: 45 }}
+        style={{ height: '100vh', background: 'black' }}
+      >
+        <Exp />
+      </Canvas>
+
       <About />
       <Experience />
       <TechStacks />
       <Projects />
       <Certificates />
-      
       <Contact />
       <Footer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
