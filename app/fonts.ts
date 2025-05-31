778aac277f3bf2b6a29e061e42ad30771cd735e3
maximistic <110153672+maximistic@users.noreply.github.com>
2025-05-31 23:19:59 +0530

    font config

---
+++ app/fonts.ts
@@ -0,0 +1,27 @@
+import { Unbounded, Syne, Bebas_Neue, Lora, Space_Grotesk } from "next/font/google";
+
+export const unbounded = Unbounded({
+    subsets: ["latin"],
+    display: "swap",
+})
+
+export const syne = Syne({
+    subsets: ["latin"],
+    display: "swap",
+})
+
+export const lora = Lora({
+    subsets: ["latin"],
+    display: "swap",
+})
+
+export const bebasNeue = Bebas_Neue({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap",
+})
+
+export const spaceGrotesk = Space_Grotesk({
+    subsets: ["latin"],
+    display: "swap",
+})
